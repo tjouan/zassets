858943d2fe3e46a224b676d3beb68ca2a463bfb5
Thibault Jouan <tj@a13.fr>
2012-10-04 22:31:38 +0000

    Add SprocketsEnv specs

---
+++ spec/zassets/sprockets_env_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+module ZAssets
+  describe SprocketsEnv do
+    let(:config)  { Config.new }
+    subject(:env) { SprocketsEnv.new(config) }
+
+    describe 'working directory' do
+      it 'initializes with current working directory' do
+        env.root.should == Dir.pwd
+      end
+    end
+
+    describe 'logger level' do
+      context 'by default' do
+        it 'defaults to fatal' do
+          env.logger.level.should == Logger::FATAL
+        end
+      end
+
+      context 'in verbose mode' do
+        let(:config) { Config.new(verbose: true)}
+
+        it 'is set to debug' do
+          env.logger.level.should == Logger::DEBUG
+        end
+      end
+    end
+
+    describe 'search paths' do
+      let(:paths)   { ['assets/scripts', 'assets/styles'] }
+      let(:config)  { Config.new(paths: paths) }
+
+      it 'registers the configured search paths' do
+        env.paths.should include(File.join(env.root, paths[0]))
+        env.paths.should include(File.join(env.root, paths[1]))
+      end
+    end
+  end
+end
