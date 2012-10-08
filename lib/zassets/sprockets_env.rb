279f6f3e7c8a92a3b8dcf4be4037194d497a422d
Thibault Jouan <tj@a13.fr>
2012-10-08 21:38:39 +0000

    Remove Handlebars and LESS integration

--- lib/zassets/sprockets_env.rb
+++ lib/zassets/sprockets_env.rb
@@ -1,9 +1,5 @@
 require 'sprockets'
 
-module Sprockets
-  autoload :LessTemplate, 'sprockets/less_template'
-end
-
 module ZAssets
   class SprocketsEnv < Sprockets::Environment
     def initialize(config)
@@ -11,13 +7,7 @@ module ZAssets
 
       self.logger.level = Logger::DEBUG if config[:verbose]
 
-      self.register_engine '.less', ::Sprockets::LessTemplate
-
       config[:paths].each { |p| append_path p }
-
-      require 'handlebars_assets'
-      append_path HandlebarsAssets.path
-      self.register_engine '.hbs', ::HandlebarsAssets::TiltHandlebars
     end
   end
 end
