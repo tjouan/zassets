7ee790220b3616cb00f9d15e94685851100cabe2
Thibault Jouan <tj@a13.fr>
2013-12-03 02:21:02 +0000

    Improve MemoryFile specs:
    * Nest into ZAssets module;
    * Use explicit subject instead of implicit one.

--- spec/zassets/memory_file_spec.rb
+++ spec/zassets/memory_file_spec.rb
@@ -1,44 +1,46 @@
 require 'spec_helper'
 
-describe ZAssets::MemoryFile do
-  include FixturesHelpers
+module ZAssets
+  describe MemoryFile do
+    include FixturesHelpers
 
-  let(:file_path) { fixture_path_for('public/index.html') }
-  let(:file)      { File.new file_path }
-  let(:subject)   { described_class.new(file) }
+    let(:file_path)   { fixture_path_for('public/index.html') }
+    let(:file)        { File.new file_path }
+    subject(:memfile) { MemoryFile.new(file) }
 
-  describe '#headers' do
-    it 'sets the content type to text/html' do
-      expect(subject.headers['Content-Type']).to eq 'text/html'
+    describe '#headers' do
+      it 'sets the content type to text/html' do
+        expect(memfile.headers['Content-Type']).to eq 'text/html'
+      end
+
+      it 'sets the content length to the file length' do
+        expect(memfile.headers['Content-Length']).to eq file.size.to_s
+      end
     end
 
-    it 'sets the content length to the file length' do
-      expect(subject.headers['Content-Length']).to eq file.size.to_s
-    end
-  end
-
-  describe '#body' do
-    it 'returns the file content' do
-      expect(subject.body).to eq File.read(file_path)
-    end
-  end
-
-  describe '#call' do
-    require 'rack'
-
-    let(:app)       { Rack::MockRequest.new(subject) }
-    let(:response)  { app.get('/') }
-
-    it 'returns a successful response' do
-      expect(response).to be_ok
+    describe '#body' do
+      it 'returns the file content' do
+        expect(memfile.body).to eq File.read(file_path)
+      end
     end
 
-    it 'sets required headers' do
-      expect(response.headers).to eq subject.headers
-    end
+    describe '#call' do
+      require 'rack'
 
-    it 'returns the body' do
-      expect(response.body).to eq subject.body
+      let(:app)       { Rack::MockRequest.new(memfile) }
+      let(:response)  { app.get '/' }
+
+      it 'returns a successful response' do
+        expect(response).to be_ok
+      end
+
+      it 'sets required headers' do
+        expect(response.headers).to eq memfile.headers
+      end
+
+      it 'returns the body' do
+        expect(response.body).to eq memfile.body
+      end
     end
   end
 end
