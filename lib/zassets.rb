71df3fd5faeb03f2826695e710878e40c5ef0058
Thibault Jouan <tj@a13.fr>
2015-07-18 14:28:18 +0000

    Remove autoload usage

--- lib/zassets.rb
+++ lib/zassets.rb
@@ -1,11 +1,9 @@
-require 'zassets/version'
 require 'coffee_script'
 
-module ZAssets
-  autoload :CLI,          'zassets/cli'
-  autoload :Builder,      'zassets/builder'
-  autoload :Config,       'zassets/config'
-  autoload :MemoryFile,   'zassets/memory_file'
-  autoload :Server,       'zassets/server'
-  autoload :SprocketsEnv, 'zassets/sprockets_env'
-end
+require 'zassets/builder'
+require 'zassets/cli'
+require 'zassets/config'
+require 'zassets/memory_file'
+require 'zassets/server'
+require 'zassets/sprockets_env'
+require 'zassets/version'
